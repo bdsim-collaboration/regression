adce123e842ebba05311c4d400cfe4b841138e68
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-08-31 11:45:51 +0100

    need directory for test_subprocess

--- 25_python/test_subprocess.py
+++ 25_python/test_subprocess.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 
 pytestmark = pytest.mark.xfail(reason="requires bdsim")
 
@@ -7,7 +8,7 @@ def func_test():
 
 def test_subprocess(make_bdsim_test_code, run_bdsim_test_code_as_subprocess):
 
-    code_to_run = make_bdsim_test_code(func_test)
+    code_to_run = make_bdsim_test_code(func_test, args="", dir=os.path.dirname(os.path.abspath(__file__)))
     output = run_bdsim_test_code_as_subprocess(code_to_run)
 
 
